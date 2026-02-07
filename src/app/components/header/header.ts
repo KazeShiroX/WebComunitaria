7f21a11655166c219a32a59d80f1bbdb06a22ca0
kevlog02 <kevinandresruro10@gmail.com>
2026-02-07 11:35:47 -0700

    cambio de iconos

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -11,6 +11,11 @@ import { AuthService } from '../../services/auth.service';
 export class Header {
   authService = inject(AuthService);
   mobileMenuOpen = signal(false);
+
+  userInitial = computed(() => {
+    const usuario = this.authService.usuario();
+    return usuario?.nombre ? usuario.nombre.charAt(0).toUpperCase() : '';
+  });
 
   toggleMobileMenu() {
     this.mobileMenuOpen.update(v => !v);
